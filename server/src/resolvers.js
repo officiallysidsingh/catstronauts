2f4469aa9e3896de69f40ccc18dd363256fd14fe
sidsinghrajput <siddharthsingh2014@gmail.com>
2023-05-10 08:49:18 +0530

    added the resolver for incrementing track views in mutation

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -16,6 +16,23 @@ const resolvers = {
       return dataSources.trackAPI.getModule(id);
     },
   },
+
+  Mutation: {
+    // increments a track's numberOfViews property
+    incrementTrackViews: async (_, { id }, { dataSources }) => {
+      const track = await dataSources.trackAPI.incrementTrackViews(id);
+
+      return {
+        code: 200,
+        success: true,
+        message: `Successfully incremented number of views for track ${encodeURIComponent(
+          id
+        )}`,
+        track,
+      };
+    },
+  },
+
   Track: {
     author: ({ authorId }, _, { dataSources }) => {
       return dataSources.trackAPI.getAuthor(authorId);
